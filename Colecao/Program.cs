ff5871514f065dc4a391186efdb2c4e70923f368
tonihenriques <antoniohenriques52@gmail.com>
2022-08-17 14:44:56 -0300

    Collection

---
+++ Colecao/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                //int[] numerosDigitado = new int[10];
+
+                //for( int i = 0; i < numerosDigitado.Length; i++)
+                //{
+                //    Console.WriteLine("Digite o número {0}: ", i + 1);
+                //    numerosDigitado[i] = Convert.ToInt32(Console.ReadLine());
+
+                //}
+
+                var numerosDigitadoList = new List<int>();
+
+                bool continuarLer = true;
+
+                while (continuarLer)
+                {
+                    Console.WriteLine("digite um número: ");
+
+                    string dadoDigitado = Console.ReadLine();
+
+                    if (dadoDigitado.Equals("sair"))
+                    {
+                        continuarLer = false;
+                    }
+                    else
+                    {
+                        numerosDigitadoList.Add(int.Parse(dadoDigitado));
+                    }
+
+                }
+
+                for (var i = 0; i < numerosDigitadoList.Count; i++)
+                {
+                    Console.WriteLine("Lista de numeros é = {0}", numerosDigitadoList[i]);
+                }
+
+                Console.ReadKey();
+
+
+            
+        }
+    }
+}
