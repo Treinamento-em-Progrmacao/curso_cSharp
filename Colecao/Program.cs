e5b324c9347695e42083d15a369b76cc43dd0dda
tonihenriques <antoniohenriques52@gmail.com>
2022-08-17 19:12:05 -0300

    CollectionRemove

--- Colecao/Program.cs
+++ Colecao/Program.cs
@@ -7,17 +7,21 @@ namespace Colecao
     {
         static void Main(string[] args)
         {
-            
-                //int[] numerosDigitado = new int[10];
 
-                //for( int i = 0; i < numerosDigitado.Length; i++)
-                //{
-                //    Console.WriteLine("Digite o número {0}: ", i + 1);
-                //    numerosDigitado[i] = Convert.ToInt32(Console.ReadLine());
+            #region aula1
 
-                //}
+            //int[] numerosDigitado = new int[10];
 
-                var numerosDigitadoList = new List<int>();
+            //for( int i = 0; i < numerosDigitado.Length; i++)
+            //{
+            //    Console.WriteLine("Digite o número {0}: ", i + 1);
+            //    numerosDigitado[i] = Convert.ToInt32(Console.ReadLine());
+
+            //}
+
+            #endregion
+
+            var numerosDigitadoList = new List<int>();
 
                 bool continuarLer = true;
 
@@ -43,10 +47,40 @@ namespace Colecao
                     Console.WriteLine("Lista de numeros é = {0}", numerosDigitadoList[i]);
                 }
 
-                Console.ReadKey();
 
+                    Console.WriteLine("Digite um numero para ser removido : ");
+
+                     string num = Console.ReadLine();
+
+                    numerosDigitadoList.Remove(Convert.ToInt32(num));
+
+
+                for (var i = 0; i < numerosDigitadoList.Count; i++)
+                {
+                    Console.WriteLine("Lista com numero removido é = {0}", numerosDigitadoList[i]);
+                }
+
+
+            Console.WriteLine("Digite um índice para ser removido : ");
+
+            string indice = Console.ReadLine();
+           
+            numerosDigitadoList.RemoveAt(Convert.ToInt32(indice));
+
+            for (var i = 0; i < numerosDigitadoList.Count; i++)
+            {
+                Console.WriteLine("Lista com índice removido é = {0}", numerosDigitadoList[i]);
+            }
 
             
+
+
+            Console.ReadKey();
+
+           
+
+
+
         }
     }
 }
